1aafc9da41b28bce9bc4afb5184e9fcc9082a46f
wula <krylovv455@gmail.com>
2020-11-26 01:45:17 +0300

    Working version with theme

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 
 
-# Press the green button in the gutter to run the script.
+class Coffee(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.setWindowTitle('Coffee description')
+        self.con = sqlite3.connect('coffee.sqlite')
+        style = f"background-color:rgb{50, 50, 50}"
+        self.centralWidget().setStyleSheet(style)
+        self.statusbar.setStyleSheet(style)
+        self.initUI()
+
+    def initUI(self):
+        cur = self.con.cursor()
+        result = cur.execute("""SELECT * FROM Coffee""").fetchall()
+        self.coffeeView.setColumnCount(len(result[0]))
+        self.coffeeView.setRowCount(0)
+
+        for i, row in enumerate(result):
+            self.coffeeView.setRowCount(
+                self.coffeeView.rowCount() + 1)
+            for j, obj in enumerate(row):
+                self.coffeeView.setItem(
+                    i, j, QTableWidgetItem(str(obj)))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Coffee()
+    ex.show()
+    sys.exit(app.exec())
